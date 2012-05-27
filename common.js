fe841da4a7b571be6987ffd713e90d73ffbc8b22
rejetto <a@rejetto.com>
2012-05-27 17:46:39 +0200

    removed duplicate files

--- common.js
+++
@@ -1,8 +0,0 @@
-require('./extending');
-GLOBAL.path = require('path');
-GLOBAL.util = require('util');
-GLOBAL.os = require('os');
-GLOBAL.fs = require('fs');
-GLOBAL.assert = require('assert');
-GLOBAL.misc = require('./misc');
-GLOBAL.ceLib = require('cloneextend');
