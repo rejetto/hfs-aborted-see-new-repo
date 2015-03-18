cf72121d4c06c7ee71a43ecdc7fe3103cb4045e1
Massimo Melina <a@rejetto.com>
2015-03-18 10:44:43 +0100

    frontend: nicer

--- static/frontend/main.js
+++ static/frontend/main.js
@@ -119,7 +119,7 @@ function loadFolder(path /** optional */, cb /** optional */) {
     if (path) currentFolder = path;
     // breadcrumbs
     $('#folder').html(
-        '<a href="/#"><img src="'+getPicURI('home')+'" style="height:1em;"/></a>'+
+        '<a href="/#"><img src="'+getPicURI('home')+'"/></a>'+
         // build pairs text/link
         decodeURI(currentFolder).split('/').filter('A').map('["/"+A, "/#"+C.slice(0,B+1).join("/")+"/"]')
         // then to html
